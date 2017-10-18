21d660fcbacbdc4973458653291d441376ea9041
vdhawal <vdhawal@adobe.com>
2017-10-18 20:41:52 +0530

    adding database support

--- routes.js
+++ routes.js
@@ -1,5 +1,24 @@
 'use strict';
 
+var fs = require("fs");
+var dbInfo = fs.readFileSync("./dbinfo.json");
+
+dbInfo = JSON.parse(dbInfo);
+
+var uri = dbInfo.uri;
+
+var MongoClient = require('mongodb').MongoClient;
+MongoClient.connect(uri, function(err, db1) {
+    if (err) {
+        console.log(err);
+    } else {
+        var col = db1.collection("userinfo");
+        col.count().then(function (count){
+            console.log("mongdb " + count);   
+        });
+    }
+});
+
 const routes = [
     {
         path: '/users',
