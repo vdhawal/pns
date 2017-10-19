bc55a782cdc4d4e8c2e45e11067cd3637c7443d3
vdhawal <vdhawal@adobe.com>
2017-10-19 14:43:27 +0530

    using the fixed id for firebase messaging

--- public/js/init.js
+++ public/js/init.js
@@ -1,6 +1,6 @@
 var config = {
             apiKey: "AIzaSyD-oetmGMXDFTGuXB332DJP49lLMlCJ-QM",
             authDomain: "adobe-notificatin-sample.firebaseapp.com",
-            messagingSenderId: "234110556796"
+            messagingSenderId: "103953800507"
         };
 firebase.initializeApp(config);
