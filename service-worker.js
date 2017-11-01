fa988dfeb026cfc65b9df5b79d4088aa53e20153
Jagjit Singh <sranjagjitsingh@gmail.com>
2017-11-01 13:10:30 +0530

    Removing onMessageEventListener from service-worker as it was added by mistake

--- service-worker.js
+++ service-worker.js
@@ -17,23 +17,6 @@ self.addEventListener('push', function(event) {
   );
 });
 
-self.addEventListener('onMessage', function(event) {
-  console.log('Received a push message', event);
-
-  var title = 'Yay a message.';
-  var body = 'We have received a push message.';
-  var icon = 'images/icon-192x192.png';
-  var tag = 'simple-push-demo-notification-tag';
-
-  event.waitUntil(
-    self.registration.showNotification(title, {
-      body: body,
-      icon: icon,
-      tag: tag
-    })
-  );
-});
-
 self.addEventListener('notificationclick', function(event) {
   console.log('On notification click: ', event.notification.tag);
   // Android doesn’t close the notification when you click on it
